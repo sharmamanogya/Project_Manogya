be44ff1b475b4a88eb7aa3daad327b03f41de484
unknown <manogyasharma2002@gmail.com>
2024-02-03 21:15:12 +0530

    first_commit

---
+++ portfolio-main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Graphic Designer", "Frontent Developer", "YouTuber", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
